3182ae4f6ae7a9dab0134da37dd29718d7ddc705
KarxiS <2karolsugar@gmail.com>
2024-01-03 22:33:09 +0100

    fix

--- CloveceSEM/Hra.cpp
+++ CloveceSEM/Hra.cpp
@@ -38,18 +38,22 @@ void Hra::zacniHru()
 
 void Hra::spravTah()
 {
-    Hrac& hracNaTahu = hraci.at(aktualnyHrac);
+    Hrac& hracNaTahu = hraci.at(0);
     //vykonanie akcie hraca
     int padnuteCislo = hracNaTahu.hodKockou(this->kocka);
     //hracNaTahu.spravPosun(hracNaTahu.figurky.at(rand()%4), padnuteCislo);
     Policko& policko = *hraciaDoska.getPolicko(1,4);
     Figurka& figurka = hracNaTahu.figurky[0];
     figurka.setAktualnePolicko(&policko);
+    Hrac& hracNaTahu2 = hraci.at(1);
+    Policko& policko2 = *hraciaDoska.getPolicko(4,4);
+    Figurka& figurka2 = hracNaTahu2.figurky[1];
+    figurka2.setAktualnePolicko(&policko2);
 
-    Policko& policko2 = *hraciaDoska.getPolicko(1,4);
+    figurka.posunSa(3);
     hraciaDoska.zobrazHraciePole();
-    figurka.posunSa(8);
-    figurka.posunSa(1);
+
+
     hraciaDoska.zobrazHraciePole();
     //aktualizacia hraca pre dalsie kolo
     this->aktualnyHrac = (this->aktualnyHrac + 1) % hraci.size();
