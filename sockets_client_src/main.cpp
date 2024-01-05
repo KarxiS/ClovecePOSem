597b1d244528b672ea678936926f1bfceeccdc89
AndreyFar <andrej.farsky@gmail.com>
2024-01-05 14:15:01 +0100

    doplenenie

--- sockets_client_src/main.cpp
+++ sockets_client_src/main.cpp
@@ -9,12 +9,13 @@ int main() {
     //Pripojenie na server
     WSADATA wsaData;
     std::string hostName = "frios2.fri.uniza.sk";
-    short port = 12499;
+    short port = 12500;
 
     struct addrinfo *result = NULL;
     struct addrinfo hints;
     int iResult;
 
+    //inicializacia WinSock kniznice
     iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
     if (iResult != 0) {
         throw std::runtime_error("WSAStartup failed with error: " + std::to_string(iResult) + "\n");
@@ -25,27 +26,28 @@ int main() {
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
 
-    // Resolve the server address and port
+    // získanie informácií o adrese pre daný sieťový host a port.
     iResult = getaddrinfo(hostName.c_str(), std::to_string(port).c_str(), &hints, &result);
     if (iResult != 0) {
         WSACleanup();
         throw std::runtime_error("getaddrinfo failed with error: " + std::to_string(iResult) + "\n");
     }
 
-    // Create a SOCKET for connecting to server
+    // vytvorenie soketu pre pripojenie na server
     SOCKET connectSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
     if (connectSocket == INVALID_SOCKET) {
         WSACleanup();
         throw std::runtime_error("socket failed with error: " + std::to_string(WSAGetLastError()) + "\n");
     }
 
-    // Connect to server
+    // pripojenie na server
     iResult = connect(connectSocket, result->ai_addr, (int)result->ai_addrlen);
     if (iResult == SOCKET_ERROR) {
         closesocket(connectSocket);
         connectSocket = INVALID_SOCKET;
     }
 
+    // uvolnenie pamäťi alokovanej pre zoznam adries
     freeaddrinfo(result);
 
     if (connectSocket == INVALID_SOCKET) {
